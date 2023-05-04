4cd320b911356d05646c4d1b6362eee31bbc1e89
Andres Castro <castrogomezand@gmail.com>
2023-05-04 17:18:44 -0500

    feat: add relation between user and task model :sparkles:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_215144) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_220612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_215144) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "owner_id", null: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
+    t.index ["owner_id"], name: "index_tasks_on_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_215144) do
   end
 
   add_foreign_key "tasks", "categories"
+  add_foreign_key "tasks", "users", column: "owner_id"
 end
